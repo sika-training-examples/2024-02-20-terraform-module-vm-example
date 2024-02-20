d48274eef08a9d9fb129d390e51fcb63203d7fb4
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-20 16:57:24 +0100

    feat: Create VM module

---
+++ vm.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.34.1"
+    }
+  }
+}
+
+variable "name" {
+  description = "The name of the droplet"
+  type        = string
+}
+
+variable "image" {
+  description = "The image to use for the droplet"
+  default     = "debian-12-x64"
+  type        = string
+}
+
+variable "size" {
+  description = "The size of the droplet"
+  default     = "s-1vcpu-1gb"
+  type        = string
+}
+
+variable "ssh_keys" {
+  description = "The SSH keys to use for the droplet"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "The VPC ID to use for the droplet"
+  type        = string
+  default     = null
+}
+
+variable "domain_id" {
+  description = "The domain to use for the droplet"
+  type        = string
+}
+
+variable "www" {
+  description = "Create a www record"
+  type        = bool
+  default     = true
+}
+
+resource "digitalocean_droplet" "this" {
+  image    = var.image
+  name     = var.name
+  region   = "fra1"
+  size     = var.size
+  ssh_keys = var.ssh_keys
+  vpc_uuid = var.vpc_id
+}
+
+resource "digitalocean_record" "this" {
+  domain = var.domain_id
+  type   = "A"
+  name   = digitalocean_droplet.this.name
+  value  = digitalocean_droplet.this.ipv4_address
+}
+
+resource "digitalocean_record" "www" {
+  count = var.www ? 1 : 0
+
+  domain = var.domain_id
+  type   = "CNAME"
+  name   = "www.${digitalocean_droplet.this.name}"
+  value  = digitalocean_record.this.fqdn
+}
+
+
+output "droplet" {
+  value = digitalocean_droplet.this
+}
+
+output "record" {
+  value = digitalocean_record.this
+}
+
+output "fqdn" {
+  value = digitalocean_record.this.fqdn
+}
